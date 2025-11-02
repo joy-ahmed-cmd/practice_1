1ab389f0d916bd9a53dc8e8a44f9975d2fac4ece
Joy Ahmed <onejoyahmed@gmail.com>
2025-11-02 08:38:01 +0600

    Comitted p2 Inkwell

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,40 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(title: Text('Practice 2'), backgroundColor: Colors.amberAccent),
+        appBar: AppBar(
+          title: Text('Practice 2'),
+          backgroundColor: Colors.lightBlue,
+        ),
+        body: InkWell(
+          onTap: () {
+            print('Tapped');
+          },
+          child: Center(
+            child: Container(
+              width: 200,
+              height: 200,
+              color: Colors.amber,
+              child: Center(
+                child: InkWell(
+                  child: Text(
+                    'Click Me',
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.green,
+                    ),
+                  ),
+                  onTap: () {
+                    print('Click on Text');
+                  },
+                  onLongPress: () {
+                    print('Long pressed ');
+                  },
+                ),
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
