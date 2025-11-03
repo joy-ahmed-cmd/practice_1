57ba9c2af7e4425bc8176ba8fca3f9e782ae61b2
Joy Ahmed <onejoyahmed@gmail.com>
2025-11-03 20:01:46 +0600

    Comitted p4 Listvew

--- lib/main.dart
+++ lib/main.dart
@@ -1,50 +1,58 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(Myapp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class Myapp extends StatelessWidget {
+  const Myapp({super.key});
 
   @override
   Widget build(BuildContext context) {
+    var arrName = [
+      'joy',
+      'halim',
+      'Khatun ',
+      'Saleha',
+      'Promita',
+      'Anjon',
+      'Ashraful',
+    ];
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Practice 2'),
-          backgroundColor: Colors.lightBlue,
-        ),
-        body: InkWell(
-          onTap: () {
-            print('Tapped');
-          },
-          child: Center(
-            child: Container(
-              width: 200,
-              height: 200,
-              color: Colors.amber,
-              child: Center(
-                child: InkWell(
-                  child: Text(
-                    'Click Me',
-                    style: TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.green,
-                    ),
+        appBar: AppBar(title: Text('Hello'), backgroundColor: Colors.blueGrey),
+        body: ListView.separated(
+          itemBuilder: (context, index) {
+            return Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    children: [
+                      Text(arrName[index], style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(arrName[index], style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),),
+                      ),
+                    ],
                   ),
-                  onTap: () {
-                    print('Click on Text');
-                  },
-                  onLongPress: () {
-                    print('Long pressed ');
-                  },
                 ),
-              ),
-            ),
-          ),
+
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(arrName[index], style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),),
+                ),
+
+
+              ],
+            );
+          },
+          itemCount: arrName.length,
+          separatorBuilder: (context, index) {
+            return Divider(height: 100, thickness: 4);
+          },
         ),
       ),
     );
