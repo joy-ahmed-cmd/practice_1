8dc92c69102a635f7ec28b2a5bc5c460574bcb14
Joy Ahmed <onejoyahmed@gmail.com>
2025-11-06 07:36:35 +0600

    Comitted p11 Card & Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:practice_1/home.dart';
 
-
-
 void main() {
   runApp(Myapp());
 }
@@ -12,14 +10,6 @@ class Myapp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        textTheme: TextTheme(
-          headlineLarge: TextStyle(fontSize: 24,fontWeight: FontWeight.bold),
-          headlineSmall: TextStyle(fontSize: 11,fontWeight: FontWeight.w500),
-        )
-      ),
-
-        home: Home());
+    return MaterialApp(home: Home());
   }
 }
